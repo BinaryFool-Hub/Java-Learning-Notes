1a9298c71b4e60c4b2debc8985b2908e8f40b0a3
BinaryFool <daishibing2006@gmail.com>
2025-04-07 19:24:20 +0800

    添加了展示转换后的数类型

--- 数据类型/引用数据类型/StringBuffer字符串/toString转换为字符串.java
+++ 数据类型/引用数据类型/StringBuffer字符串/toString转换为字符串.java
@@ -2,6 +2,9 @@ public class toString转换为字符串 {
     public static void main(String[] args) {
         StringBuffer str = new StringBuffer("这里面的是可操作数据");
         String convert = str.toString();
+
+        Class<?> type = convert.getClass();  // 展示数据类型
+        System.out.println(type);
         System.out.println(convert);
     }
 }
