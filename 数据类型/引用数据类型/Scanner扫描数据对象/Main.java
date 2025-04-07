6bc0f2d7df218ff5b1a9cb3e08361d845dd3d1ae
BinaryFool <daishibing2006@gmail.com>
2025-04-07 18:25:40 +0800

    单独列出了main.java文件中的方法

--- 数据类型/引用数据类型/Scanner扫描数据对象/Main.java
+++ 数据类型/引用数据类型/Scanner扫描数据对象/Main.java
@@ -19,16 +19,6 @@ public class Main {
         int data1 = scanner.nextInt();
         System.out.println(data1);
 
-        /* 判断输入的数据是否得到预期 */
-        System.out.println("输入整形（可以是其他类型）：");
-        if (scanner.hasNextInt()) {
-            // 如果是整形则执行这段语句，是存入判断空间再执行该代码块
-            int result = scanner.nextInt();
-            System.out.println("输入合法：" + result);
-        } else {
-            System.out.println("输入不合法");
-        }
-
         // 关闭扫描对象
         scanner.close();
     }
